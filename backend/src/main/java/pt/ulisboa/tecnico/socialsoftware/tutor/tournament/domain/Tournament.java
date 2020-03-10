043fddcf54c166120538a2c1cc1a8281164d2975
afonsoparedes <afonso.paredes@hotmail.com>
2020-03-10 13:08:45 +0000

    Tournament and TournamentDto

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/domain/Tournament.java
@@ -0,0 +1,193 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain;
+
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.QUIZ_NOT_CONSISTENT;
+
+@Entity
+@Table(
+        name = "tournaments",
+        indexes = {
+                @Index(name = "tournaments_indx_0", columnList = "key")
+        })
+public class Tournament {
+
+    public enum TournamentState {
+        OPEN, ONGOING, CLOSED
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "available_date")
+    private LocalDateTime availableDate;
+
+    @Column(name = "conclusion_date")
+    private LocalDateTime conclusionDate;
+
+    @Column(nullable = false)
+    private String title = "Title";
+
+    @Enumerated(EnumType.STRING)
+    private Tournament.TournamentState state;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tournament", fetch = FetchType.LAZY, orphanRemoval=true)
+    private Set<User> signedUsers = new HashSet<>();
+
+    @ManyToMany(mappedBy = "tournaments")
+    private Set<Topic> topics = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User tournamentCreator;
+
+    public Tournament() {}
+
+    public  Tournament(TournamentDto tournamentDto){
+
+        this.availableDate = tournamentDto.getAvailableDateDate();
+        this.conclusionDate = tournamentDto.getConclusionDateDate();
+        this.creationDate = tournamentDto.getCreationDateDate();
+        setAvailableDate(tournamentDto.getAvailableDateDate());
+        setConclusionDate(tournamentDto.getConclusionDateDate());
+        this.tournamentCreator = tournamentDto.getTournametCreator();
+        setTitle(tournamentDto.getTitle());
+        this.state = tournamentDto.getState();
+
+    }
+
+
+
+    public void setTitle(String title) {
+        checkTitle(title);
+        this.title = title;
+    }
+
+    public void setConclusionDate(LocalDateTime conclusionDate) {
+        checkConclusionDate(conclusionDate);
+        this.conclusionDate = conclusionDate;
+    }
+
+
+    public void setAvailableDate(LocalDateTime availableDate) {
+        checkAvailableDate(availableDate);
+        this.availableDate = availableDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public TournamentState getState() {
+        return state;
+    }
+
+    public void setState(TournamentState state) {
+        this.state = state;
+    }
+
+    public Set<User> getSignedUsers() {
+        return signedUsers;
+    }
+
+    public void setSignedUsers(Set<User> signedUsers) {
+        this.signedUsers = signedUsers;
+    }
+
+    public Set<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(Set<Topic> topics) {
+        this.topics = topics;
+    }
+
+    public User getTournamentCreator() {
+        return tournamentCreator;
+    }
+
+    public void setTournamentCreator(User tournamentCreator) {
+        this.tournamentCreator = tournamentCreator;
+    }
+
+    public void addTopic(Topic topic){
+        this.topics.add(topic);
+    }
+
+    public void addUser(User user){
+        this.signedUsers.add(user);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Tournament{" +
+                "id=" + id +
+                ", creationDate=" + creationDate +
+                ", availableDate=" + availableDate +
+                ", conclusionDate=" + conclusionDate +
+                ", title='" + title + '\'' +
+                ", state=" + state +
+                ", creator=" + tournamentCreator +
+                '}';
+    }
+
+
+    private void checkAvailableDate(LocalDateTime availableDate) {
+        if (this.state.equals(Tournament.TournamentState.OPEN) && availableDate == null) {
+            throw new TutorException(QUIZ_NOT_CONSISTENT, "Available date");
+        }
+        if (this.state.equals(Tournament.TournamentState.OPEN) && this.conclusionDate != null && conclusionDate.isBefore(availableDate)) {
+            throw new TutorException(QUIZ_NOT_CONSISTENT, "Available date");
+        }
+    }
+
+
+    private void checkTitle(String title) {
+        if (title == null || title.trim().length() == 0) {
+            throw new TutorException(QUIZ_NOT_CONSISTENT, "Title");
+        }
+    }
+
+    private void checkConclusionDate(LocalDateTime conclusionDate) {
+        if (this.state.equals(Tournament.TournamentState.OPEN) &&
+                conclusionDate != null &&
+                availableDate != null &&
+                conclusionDate.isBefore(availableDate)) {
+            throw new TutorException(QUIZ_NOT_CONSISTENT, "Conclusion date " + conclusionDate + availableDate);
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/dto/TournamentDto.java
@@ -0,0 +1,139 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto;
+
+import org.springframework.data.annotation.Transient;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.UserDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TournamentDto {
+    private Integer id;
+    private String title;
+    private String creationDate = null;
+    private String availableDate = null;
+    private String conclusionDate = null;
+    private Tournament.TournamentState state;
+    private List<TopicDto> topics = new ArrayList<>();
+    private User tournametCreator;
+    private int numberOfSignedUsers;
+    private int numberOfTopics;
+
+
+    @Transient
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public TournamentDto(){
+    }
+
+    public TournamentDto(Tournament tournament){
+
+    }
+
+
+    public Tournament.TournamentState getState() {
+        return state;
+    }
+
+    public void setState(Tournament.TournamentState state) {
+        this.state = state;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getAvailableDate() {
+        return availableDate;
+    }
+
+    public void setAvailableDate(String availableDate) {
+        this.availableDate = availableDate;
+    }
+
+    public String getConclusionDate() {
+        return conclusionDate;
+    }
+
+    public void setConclusionDate(String conclusionDate) {
+        this.conclusionDate = conclusionDate;
+    }
+
+    public List<TopicDto> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<TopicDto> topics) {
+        this.topics = topics;
+    }
+
+    public User getTournametCreator() {
+        return tournametCreator;
+    }
+
+    public void setTournametCreator(User tournametCreator) {
+        this.tournametCreator = tournametCreator;
+    }
+
+    public int getNumberOfSignedUsers() {
+        return numberOfSignedUsers;
+    }
+
+    public void setNumberOfSignedUsers(int numberOfSignedUsers) {
+        this.numberOfSignedUsers = numberOfSignedUsers;
+    }
+
+    public int getNumberOfTopics() {
+        return numberOfTopics;
+    }
+
+    public void setNumberOfTopics(int numberOfTopics) {
+        this.numberOfTopics = numberOfTopics;
+    }
+
+    public LocalDateTime getCreationDateDate() {
+        if (getCreationDate() == null || getCreationDate().isEmpty()) {
+            return null;
+        }
+        return LocalDateTime.parse(getCreationDate(), formatter);
+    }
+
+    public LocalDateTime getAvailableDateDate() {
+        if (getAvailableDate() == null || getAvailableDate().isEmpty()) {
+            return null;
+        }
+        return LocalDateTime.parse(getAvailableDate(), formatter);
+    }
+
+    public LocalDateTime getConclusionDateDate() {
+        if (getConclusionDate() == null || getConclusionDate().isEmpty()) {
+            return null;
+        }
+        return LocalDateTime.parse(getConclusionDate(), formatter);
+    }
+}
