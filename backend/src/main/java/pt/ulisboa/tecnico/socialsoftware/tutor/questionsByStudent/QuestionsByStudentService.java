544a91611e1d62f5021701d4a2f6f8a68a1164b3
Rafael Alexandre <r.alexandre99@gmail.com>
2020-03-11 14:23:57 +0000

    Implemented tests for F1 of PpA

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/questionsByStudent/QuestionsByStudentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/questionsByStudent/QuestionsByStudentService.java
@@ -1,9 +1,18 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.questionsByStudent;
 
+import org.springframework.stereotype.Service;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto;
+
+
+@Service
 public class QuestionsByStudentService {
 
-    public void studentSubmitQuestion() {
+    public QuestionDto studentSubmitQuestion(QuestionDto questionDto) {
+
+        return  null;
 
     }
+
 
 }
