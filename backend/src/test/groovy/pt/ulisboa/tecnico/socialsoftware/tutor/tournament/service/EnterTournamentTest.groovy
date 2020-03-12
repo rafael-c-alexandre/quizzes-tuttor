19d41d38396c57b780e7c8646c2b10fce3d7bc0f
PedroNunes99 <pedro.m.s.nunes@tecnico.ulisboa.pt>
2020-03-12 17:41:36 +0000

    Enter tournament tests defined

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/EnterTournamentTest.groovy
@@ -0,0 +1,92 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament.TournamentState
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import spock.lang.Specification
+
+import java.time.format.DateTimeFormatter
+
+@DataJpaTest
+class EnterTournamentTest extends Specification{
+    public static final String TOURNAMENT_TITLE = "Tournament"
+    public static final String CREATION_DATE = "2020-09-22 12:12"
+    public static final String AVAILABLE_DATE = "2020-09-23 12:12"
+    public static final String CONCLUSION_DATE = "2020-09-24 12:12"
+    public static final Integer ID = 2
+    public static final TournamentState STATE = TournamentState.OPEN
+    public static final User USER = new User("Pedro","Minorca",2, User.Role.STUDENT)
+
+
+    @Autowired
+    TournamentService tournamentService
+
+    @Autowired
+    TournamentRepository tournamentRepository
+
+    @Autowired
+    TopicRepository topicRepository
+
+
+
+
+
+    def formatter
+
+
+
+    def setup(){
+
+    }
+
+    def "Student enters in a closed tournament"(){
+
+        expect:false
+
+    }
+
+    def "Teacher enters in an open tournament"(){
+
+        expect:false
+
+    }
+
+    def "Admin enters in an open tournament"(){
+
+        expect:false
+
+    }
+
+    def "Demo admin enters in an open tournament"(){
+
+        expect:false
+
+    }
+
+    def "Student enters in a tournament that doesn't exist"(){
+
+        expect:false
+    }
+
+
+
+    @TestConfiguration
+    static class TournamentServiceImplTestContextConfiguration {
+
+        @Bean
+        TournamentService tournamentService() {
+            return new TournamentService()
+        }
+    }
+}
