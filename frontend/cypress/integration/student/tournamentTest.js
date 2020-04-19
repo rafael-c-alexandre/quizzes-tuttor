a141c7e3e94f201f58661223cada3b7c8573b84b
afonsoparedes <afonso.paredes@hotmail.com>
2020-04-19 19:15:17 +0100

    Fixed test

--- frontend/cypress/integration/student/tournamentTest.js
+++ frontend/cypress/integration/student/tournamentTest.js
@@ -69,22 +69,5 @@ describe('Tournament Tests', () => {
         cy.cancelTournament('Tournament Title2')
 
     });
-
-    it('login creates tournament and tries to enroll twice', () => {
-        cy.createTournament('Tournament Title2','2020-09-22 12:12','2020-10-22 12:12','5',['Adventure Builder'])
-
-        cy.enrollTournament('Tournament Title2')
-
-        cy.log('try to enroll twice')
-
-        cy.enrollTournament('Tournament Title2')
-
-        cy.closeErrorMessage()
-
-        cy.log('close dialog')
-
-        cy.get('[data-cy="closeButton"]').click()
-
-        cy.listTournaments()
-    });
+    
 });
