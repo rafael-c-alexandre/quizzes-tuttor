5e6cc5a779eace87c661822dee2a79742671f56d
Serralheiro <f.serralheiro99@gmail.com>
2020-04-19 23:01:09 +0100

    fixing tests of list open

--- frontend/cypress/integration/student/tournamentTest.js
+++ frontend/cypress/integration/student/tournamentTest.js
@@ -17,72 +17,63 @@ describe('Tournament Tests', () => {
 
     });
 
-    it('login creates two tournaments, opens them with time and confirms open tournaments list', () => {
+    it('login checks open tournaments list', () => {
 
-        cy.createTournament('Tournament Title1','2020-09-22 12:12','2020-10-22 12:12','5',['Adventure Builder'])
-        cy.createTournament('Tournament Title2','2020-09-22 12:12','2020-10-22 12:12','5',['Adventure Builder'])
-
-        now = new Date(Date.UTC(2020, 9, 22, 12, 12)).getTime()
-
-        cy.enrollTournament('Tournament Title1')
-        cy.enrollTournament('Tournament Title1')
-
-        cy.clock(now)
         cy.listOpenTournaments()
 
         //asserts
-        cy.contains('Tournament Title1')
+        cy.contains('Torneio5')
             .parent()
             .should('have.length', 2)
             .children()
             .should('have.length', 4)
 
-        cy.contains('Tournament Title2')
+        cy.contains('Torneio2')
             .parent()
             .should('have.length', 2)
             .children()
             .should('have.length', 4)
 
         cy.listTournaments()
-
-        cy.cancelTournament('Tournament Title1')
-        cy.cancelTournament('Tournament Title2')
     });
 
-    it('login creates two tournaments, closes them with time and confirms closed tournaments list, ', () => {
-
-        cy.createTournament('Tournament Title1','2020-09-22 12:12','2020-10-22 12:12','5',['Adventure Builder'])
-        cy.createTournament('Tournament Title2','2020-09-22 12:12','2020-10-22 12:12','5',['Adventure Builder'])
-
-        cy.enrollTournament('Tournament Title1')
-        cy.enrollTournament('Tournament Title1')
-
-        start_date = new Date(Date.UTC(2020, 9, 22, 12, 12)).getTime()
-        cy.clock(start_date)
-        end_date = new Date(Date.UTC(2020, 10, 22, 12, 12)).getTime()
-        cy.clock(end_date)
-
+    it('login checks closed tournaments list, ', () => {
         cy.listClosedTournaments()
 
         //asserts
-        cy.contains('Tournament Title1')
+        cy.contains('Torneio4')
             .parent()
             .should('have.length', 2)
             .children()
             .should('have.length', 4)
 
-        cy.contains('Tournament Title2')
+        cy.contains('Torneio3')
             .parent()
             .should('have.length', 2)
             .children()
-            .should('have.length', 4)
+            .should('have.length', 3)
 
         cy.listTournaments()
-
-        cy.cancelTournament('Tournament Title1')
-        cy.cancelTournament('Tournament Title2')
-
     });
+
+    it('login tries to cancel a  closed tournament', () => {
+        cy.listTournaments()
+        cy.cancelTournament("Torneio4")
+        cy.closeErrorMessage()
+    });
+
+    it('login tries to sign into a  closed tournament', () => {
+        cy.listTournaments()
+        cy.enrollTournament("Torneio4")
+        cy.closeErrorMessage()
+    });
+
+    it('login tries to sign into an open tournament', () => {
+        cy.listTournaments()
+        cy.enrollTournament("Torneio2")
+        cy.closeErrorMessage()
+    });
+
 
     it('login creates two tournaments with same name and deletes it', () => {
         cy.createTournament('Tournament Title2','2020-09-22 12:12','2020-10-22 12:12','5',['Adventure Builder'])
